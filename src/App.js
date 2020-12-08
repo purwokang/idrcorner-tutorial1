d3ef15c8f3f50dacf930bd3efa2de651280e96d3
purwokang <purwo.com@gmail.com>
2020-12-08 13:38:48 +0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <h1>Halo, Aplikasi React Pertama</h1>
+        <h1>Daftar Produk</h1>
         <Produk produkProduk={this.state.produk} />
       </div>
     );
--- src/Produk.js
+++ src/Produk.js
@@ -1,27 +1,30 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Produk extends Component {
-    render() {
+const Produk = ({ produkProduk }) => {
 
-        // console.log(this.props);
+    const listProduk = produkProduk.map(produk => {
 
-        const { produkProduk } = this.props
-        const listProduk = produkProduk.map(produk => {
-            return (
-                <div className="kotak" key={produk.id}>
+        let minstok = produk.stok <= 30 ? ('minstok') : (null)
+
+        return (
+            <div className="card kotak">
+                <div className={minstok + " card"} key={produk.id}>
                     <p>Nama Produk: {produk.namaProduk} </p>
                     <p>Harga: {produk.harga} </p>
                     <p>Stok: {produk.stok} </p>
                 </div>
-            )
-        })
-
-        return (
-            <div>
-                {listProduk}
             </div>
-        );
-    }
+        )
+
+    })
+
+    return (
+        <div>
+            {listProduk}
+        </div>
+    );
+
 }
 
 export default Produk;
+
